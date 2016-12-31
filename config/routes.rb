4a153decb5561c7e68a10c631070aef88bba2a31
Josh Pann <josh.pann@example.com>
2016-12-31 15:08:38 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  ##resources creates routes, aliases for helper methods like _path, and links to controller action. these key word know to seperate the get request for the form and the post/put/ request for the database manipulating.
+  ##rendering form is done in the get request (ie :new), manipulating is a seperate request/route (ie :create).
 end
